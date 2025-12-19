8168fa4249c89c9e17e399ee050db325d76a67e6
harwie1962 <hspace@gmx.de>
2025-12-19 15:20:23 +0100

    Update mon.js

--- plans/mon.js
+++ plans/mon.js
@@ -1,4 +1,4 @@
 const schedule = [
   { time: "06:15", title: "Prednisolon", description: "", calories: 0 },
-  { time: "07:00", title: "Frühstück", description: "Beispiel", calories: 300 }
+  // ...
 ];
