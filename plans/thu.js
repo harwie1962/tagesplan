29b20dd208abbcc20c505230786d9e8799bed574
harwie1962 <hspace@gmx.de>
2025-12-28 23:11:54 +0100

    add thursday plan

--- plans/thu.js
+++ plans/thu.js
@@ -1,28 +1,35 @@
 // plans/thu.js
 window.schedule = [
+  { time: "06:00", title: "Apfelessig-Wasser", description: "optional: 1 TL in Wasser, 10–15 Min Abstand zu Medikamenten", calories: 0 },
+  { time: "06:10", title: "Tagesfokus", description: "🟢 Donnerstag – Darm- & Regenerationstag: Verdauung entlasten, abends extra Ruhe", calories: 0 },
+
   { time: "06:15", title: "Prednisolon", description: "", calories: 0 },
   { time: "06:30", title: "Vitamin-B-Komplex", description: "", calories: 0 },
   { time: "06:40", title: "Magnesium-Malat", description: "", calories: 0 },
   { time: "06:45", title: "L-Glutamin", description: "5 g (mit Wasser)", calories: 0 },
 
   { time: "07:00", title: "Medikamente + Supplemente (morgens)", description: "Eliquis/Apixaban 5 mg + Finerenon 10 mg + Vitamin D3/K2 + Omega-3 + Q10", calories: 0 },
-  
   { time: "07:30", title: "Kollagen-Drink", description: "10 g", calories: 36 },
-  { time: "08:00", title: "Frühstück (klassisch)", description: "Haferflocken 50 g, Magerquark 150 g, Beeren 80 g, Leinöl 1 EL, Flohsamen 1 TL", calories: 350 },
-  { time: "08:10", title: "Vitamin C", description: "200–250 mg, zu/kurz nach dem Frühstück", calories: 0 },
+
+  { time: "08:00", title: "Frühstück (warm oder sehr leicht)", description: "Haferflocken 50 g, Magerquark 150 g, Beeren 80 g, Leinöl 1 EL, Flohsamen 1 TL (bei Bedarf wärmer/leichter gestalten)", calories: 350 },
+  { time: "08:00", title: "Vitamin C", description: "200–250 mg, zu/kurz nach dem Frühstück", calories: 0 },
+
   { time: "09:00", title: "Herz / Niere", description: "Bisoprolol, Amlodipin, Ezetimib", calories: 0 },
   { time: "09:30", title: "Eiweißblock (klein)", description: "Magerquark 100 g + 1 Ei (gekocht)", calories: 150 },
 
-  { time: "12:30", title: "Hauptmahlzeit (gekocht, ggf. abgekühlt)", description: "Kartoffeln 250 g, Gemüse 300 g (Bohnen/Karotten), Hähnchenbrust 120 g, Olivenöl 1 TL", calories: 420 },
+  { time: "12:15", title: "Apfelessig-Wasser", description: "optional: vor dem Mittagessen", calories: 0 },
+  { time: "12:30", title: "Mittag normal", description: "Kartoffeln 250 g, Gemüse 300 g (Bohnen/Karotten), Hähnchenbrust 120 g, Olivenöl 1 TL", calories: 420 },
   { time: "13:00", title: "Artischocke", description: "Leber / Galle", calories: 0 },
 
   { time: "15:30", title: "Kleine Kohlenhydratportion", description: "Haferflocken 30 g + Wasser oder Tee", calories: 115 },
   { time: "17:30", title: "L-Glutamin (2. Portion)", description: "3–5 g", calories: 0 },
 
   { time: "18:00", title: "Abendmedikamente", description: "Eliquis/Apixaban 5 mg + Rosuvastatin + Ramipril", calories: 0 },
-  { time: "18:30", title: "Leicht & warm", description: "Linsen (gekocht) 1 Portion + Gemüse 200–250 g + Miso-Dashi 1 TL", calories: 240 },
+  { time: "18:30", title: "Abend sehr einfach", description: "Linsen (gekocht) 1 Portion + Gemüse 200–250 g + Miso-Dashi 1 TL (simpel, warm)", calories: 240 },
+
+  { time: "19:30", title: "Extra Ruhe", description: "Lesen / Wärme / ruhig werden (Regeneration)", calories: 0 },
 
   { time: "20:30", title: "Zink", description: "nur mit Wasser", calories: 0 },
-  { time: "20:40", title: "Optional (wenn Hunger)", description: "Magerquark 100 g", calories: 70 },
+  { time: "20:30", title: "Optional (wenn Hunger)", description: "Magerquark 100 g", calories: 70 },
   { time: "22:00", title: "Magnesium-Bisglycinat", description: "", calories: 0 }
 ];
