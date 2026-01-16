9f6b7ecd72253ce7428d7decd3ebb60c96f2eeef
harwie1962 <hspace@gmx.de>
2026-01-16 09:34:32 +0100

    add monday plan

--- plans/mon.js
+++ plans/mon.js
@@ -1,37 +1,28 @@
 // plans/mon.js
 window.schedule = [
-  { time: "06:00", title: "Apfelessig-Wasser", description: "optional: 1 TL in Wasser, 10–15 Min Abstand zu Medikamenten", calories: 0 },
-  { time: "06:10", title: "Tagesfokus", description: "Montag – Stabiler Start: ruhig bleiben, keine Experimente", calories: 0 },
-
-  { time: "06:15", title: "Prednisolon", description: "", calories: 0 },
-  { time: "06:30", title: "Vitamin-B-Komplex", description: "", calories: 0 },
-  { time: "06:40", title: "Magnesium-Malat", description: "", calories: 0 },
-  { time: "06:45", title: "L-Glutamin", description: "5 g (mit Wasser)", calories: 0 },
-
-  { time: "07:00", title: "Medikamente + Supplemente (morgens)", description: "Eliquis/Apixaban 5 mg + Finerenon 10 mg + Vitamin D3/K2 + Omega-3 + Q10", calories: 0 },
   { time: "07:30", title: "Kollagen-Drink", description: "10 g", calories: 36 },
 
   { time: "08:00", title: "Frühstück (normal)", description: "Haferflocken 50 g, Magerquark 150 g, Beeren 80 g, Leinöl 1 EL, Flohsamen 1 TL", calories: 350 },
-  { time: "08:00", title: "Vitamin C", description: "200–250 mg, zu/kurz nach dem Frühstück", calories: 0 },
 
-  { time: "09:00", title: "Herz / Niere", description: "Bisoprolol, Amlodipin, Ezetimib", calories: 0 },
+  { time: "08:10", title: "Medikamente + Supplemente (nach dem Frühstück)", description: "Prednisolon + Eliquis/Apixaban 5 mg + Finerenon 10 mg + Bisoprolol + Amlodipin + Ezetimib + Vitamin D3/K2 + Omega-3 + Q10 + Vitamin-B-Komplex + Magnesium-Malat", calories: 0 },
+  { time: "08:10", title: "Vitamin C", description: "200–250 mg, nach dem Frühstück", calories: 0 },
+
   { time: "09:30", title: "Eiweißblock (klein)", description: "1 Ei (gekocht) + Magerquark 100 g", calories: 150 },
 
-  { time: "12:15", title: "Apfelessig-Wasser", description: "optional: vor dem Mittagessen", calories: 0 },
   { time: "12:30", title: "Mittag (Rohkost/Salat erlaubt)", description: "Kartoffeln 250 g, Brokkoli 300 g, Hähnchenbrust 120 g, Olivenöl 1 TL (optional Rohkost/Salat dazu)", calories: 420 },
-  { time: "13:00", title: "Artischocke", description: "Leber / Galle", calories: 0 },
-  { time: "13:10", title: "Spaziergang", description: "10–20 Min nach dem Mittagessen", calories: 0 },
+  { time: "12:45", title: "Artischocke (nach dem Mittagessen)", description: "Leber / Galle", calories: 0 },
+  { time: "13:00", title: "Spaziergang", description: "10–20 Min nach dem Mittagessen", calories: 0 },
 
   { time: "15:30", title: "Kleine Kohlenhydratportion", description: "Haferflocken 30 g + Wasser oder Tee", calories: 115 },
-  { time: "17:30", title: "L-Glutamin (2. Portion)", description: "3–5 g", calories: 0 },
 
-  { time: "18:00", title: "Abendmedikamente", description: "Eliquis/Apixaban 5 mg + Rosuvastatin + Ramipril", calories: 0 },
+  { time: "17:30", title: "L-Glutamin", description: "3–5 g", calories: 0 },
+
   { time: "18:30", title: "Abendessen (warm & leicht)", description: "Buchweizen (gekocht) 60 g + Gemüse 250 g + Miso-Dashi 1–2 TL (spätestens 18:30)", calories: 220 },
+  { time: "18:45", title: "Abendmedikamente (nach dem Abendessen)", description: "Eliquis/Apixaban 5 mg + Rosuvastatin + Ramipril", calories: 0 },
   { time: "18:50", title: "Spaziergang", description: "10–20 Min nach dem Abendessen", calories: 0 },
 
-  { time: "20:30", title: "Zink", description: "nur mit Wasser", calories: 0 },
   { time: "20:30", title: "Optional (wenn Hunger)", description: "Magerquark 100 g", calories: 70 },
+  { time: "20:45", title: "Zink", description: "nach Abendessen, nur mit Wasser", calories: 0 },
 
-  { time: "21:30", title: "Atemtechnik", description: "4/6-Atmung (4 s ein / 6 s aus) 5–10 Min", calories: 0 },
-  { time: "22:00", title: "Magnesium-Bisglycinat", description: "", calories: 0 }
+  { time: "22:00", title: "Magnesium-Bisglycinat", description: "abends, magenschonend", calories: 0 }
 ];
