c61d8423a13a7ac89bdab739a9f9db7d4d4a9fa7
harwie1962 <hspace@gmx.de>
2026-02-03 06:55:54 +0100

    add friday plan

--- plans/fri.js
+++ plans/fri.js
@@ -2,14 +2,32 @@
 window.schedule = [
   { time: "07:30", title: "Kollagen-Drink", description: "10 g", calories: 36 },
 
-  { time: "08:00", title: "Frühstück (normal)", description: "Haferflocken 50 g, Magerquark 150 g, Beeren 80 g, Leinöl 1 EL, Flohsamen 1 TL", calories: 350 },
+  {
+    time: "08:00",
+    title: "🌅 Frühstück – leberfreundlich & sättigend",
+    description:
+      "➡ <a href='rezepte/aktuell.html#fruehstueck' target='_blank'>Varianten ansehen</a>",
+    calories: 350
+  },
 
-  { time: "08:10", title: "Medikamente + Supplemente (nach dem Frühstück)", description: "Prednisolon + Eliquis/Apixaban 5 mg + Finerenon 10 mg + Bisoprolol + Amlodipin + Ezetimib + Vitamin D3/K2 + Omega-3 + Q10 + Vitamin-B-Komplex + Magnesium-Malat", calories: 0 },
+  {
+    time: "08:10",
+    title: "Medikamente + Supplemente (nach dem Frühstück)",
+    description:
+      "Prednisolon + Eliquis/Apixaban 5 mg + Finerenon 10 mg + Bisoprolol + Amlodipin + Ezetimib + Vitamin D3/K2 + Omega-3 + Q10 + Vitamin-B-Komplex + Magnesium-Malat",
+    calories: 0
+  },
   { time: "08:10", title: "Vitamin C", description: "200–250 mg, nach dem Frühstück", calories: 0 },
 
   { time: "09:30", title: "Eiweißblock (klein)", description: "1 Ei (gekocht) + Magerquark 100 g", calories: 150 },
 
-  { time: "12:30", title: "Mittag (Rohkost/Salat erlaubt)", description: "Kartoffeln 250 g, Brokkoli 300 g, Hähnchenbrust 120 g, Olivenöl 1 TL (optional Rohkost/Salat dazu)", calories: 420 },
+  {
+    time: "12:30",
+    title: "🍽️ Mittag – ausgewogen",
+    description:
+      "➡ <a href='rezepte/aktuell.html#mittag' target='_blank'>Varianten ansehen</a>",
+    calories: 420
+  },
   { time: "12:45", title: "Artischocke (nach dem Mittagessen)", description: "Leber / Galle", calories: 0 },
   { time: "13:00", title: "Spaziergang", description: "10–20 Min nach dem Mittagessen", calories: 0 },
 
@@ -17,7 +35,13 @@ window.schedule = [
 
   { time: "17:30", title: "L-Glutamin", description: "3–5 g", calories: 0 },
 
-  { time: "18:30", title: "Abendessen (warm & leicht)", description: "Buchweizen (gekocht) 60 g + Gemüse 250 g + Miso-Dashi 1–2 TL (spätestens 18:30)", calories: 220 },
+  {
+    time: "18:30",
+    title: "🌙 Abendessen – warm & leicht",
+    description:
+      "➡ <a href='rezepte/aktuell.html#abend' target='_blank'>Varianten ansehen</a>",
+    calories: 220
+  },
   { time: "18:45", title: "Abendmedikamente (nach dem Abendessen)", description: "Eliquis/Apixaban 5 mg + Rosuvastatin + Ramipril", calories: 0 },
   { time: "18:50", title: "Spaziergang", description: "10–20 Min nach dem Abendessen", calories: 0 },
 
